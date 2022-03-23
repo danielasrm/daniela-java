92bd38fac3d63e3870bd89bdb8b05bff3e2c9069
mayvelardez95 <101293052+mayvelardez95@users.noreply.github.com>
2022-03-23 20:40:20 -0300

    daniela suma resta

--- javascript/general.js
+++ javascript/general.js
@@ -1,1 +1,11 @@
-console.log("hola mundo")
+// let resultado = suma(1,2)
+
+function  sumar (num1, num2){
+    return num1 + num2;
+ }
+var algo = resta(9,3)
+console.log(algo)
+function resta(num1,num2){
+    let resultado = num1 - num2;
+    return resultado;
+}
